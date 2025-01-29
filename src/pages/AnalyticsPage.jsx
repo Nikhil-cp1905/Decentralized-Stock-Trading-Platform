840c5b4cc58e0b49fbda974b4899c0af865b7fa3
Nikhil CP <cpnikhil05@gmail.com>
2025-01-29 17:36:54 +0530

    changed pages

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -11,20 +11,20 @@ import AIPoweredInsights from "../components/analytics/AIPoweredInsights";
 const AnalyticsPage = () => {
 	return (
 		<div className='flex-1 overflow-auto relative z-10 bg-black'>
-			<Header title={"Analytics Dashboard"} className="text-green-500" />
+			<Header title={"Analytics Dashboard"} className="text-[#182434]" />
 
 			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
-				<OverviewCards className="bg-green-900 text-white" />
-				<RevenueChart className="bg-green-800 text-white" />
+				<OverviewCards className="bg-[#182434] text-white" />
+				<RevenueChart className="bg-[#182434] text-white" />
 
 				<div className='grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8'>
-					<ChannelPerformance className="bg-green-700 text-white" />
-					<ProductPerformance className="bg-green-700 text-white" />
-					<UserRetention className="bg-green-700 text-white" />
-					<CustomerSegmentation className="bg-green-700 text-white" />
+					<ChannelPerformance className="bg-[#182434] text-white" />
+					<ProductPerformance className="bg-[#182434] text-white" />
+					<UserRetention className="bg-[#182434] text-white" />
+					<CustomerSegmentation className="bg-[#182434] text-white" />
 				</div>
 
-				<AIPoweredInsights className="bg-green-600 text-white" />
+				<AIPoweredInsights className="bg-[#182434] text-white" />
 			</main>
 		</div>
 	);
